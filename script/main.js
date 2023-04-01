9ef6b96ef7718ddd4eb956270acb033af86db92b
m-saleh96 <scyllies@gmail.com>
2023-04-01 14:50:49 +0200

    move baddle with arrows and mouse

--- script/main.js
+++ script/main.js
@@ -4,22 +4,35 @@ const medium = document.getElementById('medium');
 const difficult = document.getElementById('difficult');
 var ballSpeed;
 
-var canvas = document.getElementById("canvas");
-var ctx = canvas.getContext("2d");
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+const BG_IMG = new Image()
+BG_IMG.src = "images/main1.jpg"
 
 // select level and return ballSpeed
 easy.addEventListener('click', function () {
     level.style = "display : none;"
     ballSpeed =10;
+    loop()
 })
 medium.addEventListener('click', function () {
     level.style = "display : none;"
     ballSpeed =15;
+    loop()
 })
 difficult.addEventListener('click', function () {
     level.style = "display : none;"
     ballSpeed =20;
+    loop()
 })
+
+//------------------loop function----------------- 
+function loop() {
+    ctx.drawImage(BG_IMG,0,0)
+    draw()
+    update()
+    requestAnimationFrame(loop)
+}
 
 //------------------create paddle-----------------------------
 let paddleWidth = 120;
@@ -40,4 +53,51 @@ function drawPaddle() {
     ctx.strokeStyle = "#FFFF00"
     ctx.strokeRect(paddle.x,paddle.y,paddle.width,paddle.height)
 }
-drawPaddle()
+
+//----------------draw Function------------------
+function draw() {
+    drawPaddle()
+}
+
+
+//-----------------controll paddle---------
+let leftArrow = false;
+let rightArrow = false;
+document.addEventListener('keydown',function (e) {
+    if (e.keyCode == 37) {
+        leftArrow = true;
+    } else if(e.keyCode == 39){
+        rightArrow = true;
+    }
+})
+document.addEventListener('keyup',function (e) {
+    if (e.keyCode == 37) {
+        leftArrow = false;
+    } else if(e.keyCode == 39){
+        rightArrow = false;
+    }
+})
+document.addEventListener('mousemove',function mouseMoveHandler(e) {
+    var relativeX = e.clientX - canvas.offsetLeft;
+    if(relativeX > paddleWidth/2 && relativeX + paddleWidth/2 < canvas.width) {
+        paddle.x = relativeX - paddleWidth/2;
+    }
+    console.log(e)
+  })
+
+function movePaddle() {
+    if (rightArrow && paddle.x + paddleWidth < canvas.width) {
+        paddle.x += paddle.dx
+    } else if(leftArrow && paddle.x>0){
+        paddle.x -= paddle.dx
+    }
+}
+    
+
+//----------------update function for logic------------
+function update() {
+    movePaddle()
+}
+
+
+
