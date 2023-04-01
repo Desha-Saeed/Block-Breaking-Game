ba727373c63fe23ad4301f36290ed9b0556ce8bc
m-saleh96 <scyllies@gmail.com>
2023-04-01 10:47:52 +0200

    style of game

---
+++ script/main.js
@@ -0,0 +1,8 @@
+var level = document.getElementById('level')
+var easy = document.getElementById('easy')
+var medium = document.getElementById('medium')
+var difficult = document.getElementById('difficult')
+
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+
